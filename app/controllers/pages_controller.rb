eae5911169e5d46e5c959a017752bf84dbec70b8
Pedro Coelho <punnie@ethereal.io>
2017-06-24 22:18:18 +0100

    Create hero page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   def index
     redirect_to company_path     if company_signed_in?
     redirect_to association_path if association_signed_in?
+
+    render layout: 'hero'
   end
 end
