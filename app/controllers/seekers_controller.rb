1f344ebfdb62402e00569f07d675ac9d73de6d0a
Tadas Sce <tadas@sce.lt>
2017-06-24 23:03:55 +0100

    Add SeekersController

---
+++ app/controllers/seekers_controller.rb
@@ -0,0 +1,38 @@
+class SeekersController < ApplicationController
+  def index
+    @seekers = Seeker.where(institution_id: params[:institution_id]).all
+  end
+
+  def new
+    @seeker = current_institution.seekers.build
+  end
+
+  def create
+    @seeker = current_institution.seekers.build(seekers_params)
+    if @seeker.save
+      redirect_to @seeker
+    else
+      render :new
+    end
+  end
+
+  def show
+    @seeker = current_institution.seekers.find(params[:id])
+  end
+
+  def edit
+    @seeker = current_institution.seekers.find(params[:id])
+  end
+
+  def update
+    @seeker = current_institution.seekers.find(params[:id])
+    @seeker.update!(seekers_params)
+    redirect_to @seeker
+  end
+
+  private
+
+  def seekers_params
+    params.require(:seeker).permit(:name, :country, :city)
+  end
+end
