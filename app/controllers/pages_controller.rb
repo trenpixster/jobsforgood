bc96c2f08d80f9010b738487948a215dbde88e48
Tadas Sce <tadas@sce.lt>
2017-06-24 23:03:03 +0100

    Fix multiple render/redirects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
-    redirect_to company_path     if company_signed_in?
-    redirect_to institution_path if institution_signed_in?
+    return redirect_to company_path     if company_signed_in?
+    return redirect_to institution_path if institution_signed_in?
 
     render layout: 'hero'
   end
