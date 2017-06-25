6b58dbc7a5430118572aca5fef75cbeb553b1369
Tadas Sce <tadas@sce.lt>
2017-06-25 01:38:40 +0100

    Show matching candidates on job page

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,11 @@ class Job < ApplicationRecord
   belongs_to :company
 
   acts_as_taggable_on :skills, :languages
+
+  def at_least_one_match_for_institution(institution)
+    @at_least_one_match_for_institution ||=
+      institution.seekers.select do |seeker|
+        (language_list & seeker.language_list).any? || (skill_list & seeker.skill_list).any?
+      end
+  end
 end
