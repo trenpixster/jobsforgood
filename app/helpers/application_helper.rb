30c609878557ea1fb136355cd52fc0f5f184377d
Tadas Sce <tadas@sce.lt>
2017-06-25 10:26:08 +0100

    Add main links in the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sign_out_path
+    if institution_signed_in?
+      destroy_institution_session_path
+    elsif company_signed_in?
+      destroy_company_session_path
+    end
+  end
 end
