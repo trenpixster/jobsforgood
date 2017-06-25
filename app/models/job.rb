93a583d0143356b0d8ef3813f69f1c1d3569b9f8
Nizar Venturini <nizar.venturini@gmail.com>
2017-06-25 12:36:47 +0100

    Allow no skills and no language requirements match

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,7 +6,7 @@ class Job < ApplicationRecord
   def at_least_one_match_for_institution(institution)
     @at_least_one_match_for_institution ||=
       institution.seekers.select do |seeker|
-        (language_list & seeker.language_list).any? || (skill_list & seeker.skill_list).any?
+        (language_list & seeker.language_list).any? || (skill_list & seeker.skill_list).any? || skill_list.empty? && language_list.empty?
       end
   end
 end
